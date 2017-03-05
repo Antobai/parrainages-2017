01cc2daff2666b6665a2a91d640f3b46649e16d6
antonin <a.bailly@onlineformapro.com>
2017-03-05 16:42:28 +0100

    route pour chaque departement ok, /departement/{id}

--- app/Http/Controllers/DepartementController.php
+++ app/Http/Controllers/DepartementController.php
@@ -4,14 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Departement;
+use App\Individu;
+use App\Candidat;
 
 class DepartementController extends Controller
 {
     public function showDepartement($id) {
-   			
    		$departement = new Departement;
+   		$departement = $departement::select("nom")->where('id', $id)->first();
+   		if($departement) {
+   			$nom = $departement->nom;
+   		}
 
-   		return view('departement', array('id' => $id, 'nom' => $candidat[0]->nom,
-        'prenom'=>$candidat[0]->prenom, 'parrains' => $parrains));
+   		$individu = new Individu;
+   		$parrains = $individu::select("*")->where('id_departement', $id)->get();
+
+   		$candidat = new Candidat;
+   		foreach ($parrains as $key => $parrain) {
+
+   			$candidatResult = $candidat::select("nom","prenom")->where('id', $parrain->id_candidat)->first();
+
+   			if($candidatResult) {
+   				$parrain->nomCandidat = $candidatResult->nom;
+   				$parrain->prenomCandidat = $candidatResult->prenom;
+   			}
+   		}
+   		
+
+   	
+
+   		return view('departement', array('id' => $id,'nom'=>$nom, 'parrains' => $parrains));
    	}
 }
